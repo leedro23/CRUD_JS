41d950c8938c7819a2caa96053c3e98b4fbecb33
Leo <leonardofelixfelix@gmail.com>
2020-10-19 21:48:24 -0300

    Primeiros passos do CRUD em JavaScript

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+
+app.listen(3000, function (){
+    console.log('Server funcionando na porta 3000')
+})
+
+app.get('/', (req, res) => {
+    res.render ('index.ejs')
+})
+
+app.set('view engine', 'ejs')
+
+/*app.post('/show', (req, res) => {
+    console.log(req.body.name)
+})*/
+
+app.post('\show', function(req, res){
+    console.log(req.body)
+})
+
+app.use (bodyParser.urlencoded({extended: true}))
